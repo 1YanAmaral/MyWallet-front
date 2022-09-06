9186e66005c6d49405a8996d7a6844586bf96928
Yan Amaral <yan@pop-os.localdomain>
2022-09-06 11:00:30 -0300

    Layout tela de login ok

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,71 @@
-import { Wrapper, Logo } from "../styles/sharedStyles";
+import {
+  Wrapper,
+  Logo,
+  Info,
+  Bigbutton,
+  Content,
+  SpanLink,
+} from "../styles/sharedStyles";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleForm(e) {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+    console.log(form);
+  }
+
+  /* function sendLogin(e) {
+    e.preventDefault();
+
+    //const promise = signup(form);
+    promise.then((res) => {
+      console.log("FOI", res);
+
+      navigate("/");
+    });
+    promise.catch(() => alert("Algo está errado, verifique suas informações!"));
+  } */
   return (
-    <Wrapper>
-      <Logo>MyWallet</Logo>
-    </Wrapper>
+    <>
+      <Content>
+        <Wrapper>
+          <Logo>MyWallet</Logo>
+
+          <form onSubmit={() => {}}>
+            <Wrapper>
+              <Info
+                type="email"
+                placeholder="E-mail"
+                name="email"
+                onChange={handleForm}
+                value={form.email}
+                required
+              />
+              <Info
+                type="password"
+                placeholder="Senha"
+                name="password"
+                onChange={handleForm}
+                value={form.password}
+                required
+              />
+
+              <Bigbutton type="submit">Entrar</Bigbutton>
+            </Wrapper>
+          </form>
+          <SpanLink>Primeira vez? Cadastre-se!</SpanLink>
+        </Wrapper>
+      </Content>
+    </>
   );
 }
--- src/styles/sharedStyles.js
+++ src/styles/sharedStyles.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -17,6 +25,55 @@ const Logo = styled.div`
   font-weight: 400;
   font-size: 32px;
   color: #ffffff;
+  margin-bottom: 25px;
 `;
 
-export { Wrapper, Logo };
+const Info = styled.input`
+  font-family: "Raleway", sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 19.976px;
+  line-height: 25px;
+  color: #666666;
+  width: 303px;
+  height: 45px;
+  background-color: #ffffff;
+  border: 1px solid #d5d5d5;
+  border-radius: 5px;
+  margin: 6px;
+
+  ::placeholder {
+    font-family: "Raleway", sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 19.976px;
+    line-height: 25px;
+    color: black;
+    padding-left: 10px;
+  }
+`;
+
+const Bigbutton = styled.button`
+  width: 303px;
+  height: 45px;
+  background-color: #a328d6;
+  border-radius: 4.63636px;
+  font-family: "Raleway", sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20.976px;
+  color: #ffffff;
+  border: none;
+`;
+const SpanLink = styled.span`
+  font-family: "Raleway", sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 18px;
+  text-align: center;
+  color: #ffffff;
+  margin: 25px;
+`;
+
+export { Content, Wrapper, Logo, Info, Bigbutton, SpanLink };
